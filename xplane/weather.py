f66226f690c5d5c45f59834d9958996e89e917d4
john <johnathanchewy@gmail.com>
2021-01-04 13:50:00 -0800

    fix dictionary getattr bug

--- xplane/weather.py
+++ xplane/weather.py
@@ -11,16 +11,16 @@ condition_drefs = ["sim/weather/runway_friction",
                    "sim/weather/wind_speed_kt[0]"]
 
 
-def run_sampler(sampler, samples=10):
+def run_sampler(sampler, xp_client, samples=10):
 
     for _ in range(samples):
         scene, _ = sampler.generate()
         params = scene.params
 
-        wind_speed = getattr(params, 'wind_speed', 0)
-        wind_direction = getattr(params, 'wind_direction', 0)
-        friction = getattr(params, 'runway_friction', 0)
-
+        wind_speed = scene.params['wind_speed']
+        wind_direction = scene.params['wind_direction']
+        friction = scene.params['friction']
+        print(wind_speed, wind_direction, friction)
         xp_client.sendDREFs(condition_drefs, [friction, wind_direction, wind_speed])
         time.sleep(5)
 
@@ -29,6 +29,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--scenario', help='Scenic scenario to run', default='conditions.sc')
     args = parser.parse_args()
-    sampler = translator.scenarioFromFile(scenario)
+    sampler = translator.scenarioFromFile(args.scenario)
     client = XPlaneConnect()
-    run_sampler(sampler, samples=10)
+    run_sampler(sampler, client, samples=10)
