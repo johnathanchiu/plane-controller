1e39979c6337dc81e341dde8d4b1f19c66cab775
john <johnathanchewy@gmail.com>
2021-01-06 00:10:23 -0800

    add weather conditions

--- xplane/main.py
+++ xplane/main.py
@@ -1,13 +1,14 @@
 from solver import solve_states
 from controller import apply_controls, PID
 from definitions import XPlaneDefs
+from geometry import kn_to_ms
 
 from xpc import XPlaneConnect
+import scenic.syntax.translator as translator
 
 import numpy as np
 import math
 import time
-
 import sys
 
 ### runway setup ###
@@ -28,18 +29,45 @@ desired_z = runway_end_z
 desired_velocity = 27 # m/s
 
 acceleration_constraint = 7 # m/s^2
-turning_constraint = 10 # degrees
+turning_constraint = 60 # degrees
 
-time_step = 1
+# solver time step
+time_step = 1 # seconds
+# solver number of states to solve
 num_steps = 7
 
+# controller recompute time step
 sample_time = 0.1
+# number of seconds to run the controller + solver
 simulation_steps = 100
 
+# recompute using solver after t seconds
 receding_horizon = 5
+
 
 # create XPC object
 xp_client = XPlaneConnect()
+
+### Scenic program setup ###
+sampler = translator.scenarioFromFile('conditions.sc')
+scene, _ = sampler.generate()
+
+params = scene.params
+
+wind_speed = scene.params['wind_speed']
+wind_direction = scene.params['wind_direction']
+# friction = scene.params['friction']
+wind_speed = 0
+wind_degrees = -40
+wind_direction = runway_heading + wind_degrees
+wind_direction += 180 # wind is counter clockwise of true north
+
+friction = 0
+xp_client.sendDREFs(XPlaneDefs.condition_drefs, [friction, wind_direction, wind_speed])
+
+wind_speed = kn_to_ms(wind_speed)
+wind_direction += -2 * wind_degrees # since positive rotation is the right
+wind_direction -= 180
 
 ### initialize starting states ###
 
@@ -62,17 +90,21 @@ xp_client.sendDREF("sim/flightmodel/controls/parkbrake", 0)
 time.sleep(1)
 
 controls = None
-throttle_controller = PID(1.0, 0.0, 3.0, 20.0, sample_time)
-rudder_controller = PID(0.4, 1.0, 0.2, 20.0, sample_time)
+throttle_controller = PID(1.0, .0, 3.0, 10.0, sample_time)
+rudder_controller = PID(0.01, 1.5, 0.7, 10.0, sample_time)
 for t in range(int(simulation_steps // receding_horizon)):
     read_drefs = XPlaneDefs.control_dref + XPlaneDefs.position_dref
-    gs, psi, x, _, z = np.squeeze(np.array(xp_client.getDREFs(read_drefs)))
+    gs, psi, throttle, x, _, z = xp_client.getDREFs(read_drefs)
+    gs, psi, throttle, x, z = gs[0], psi[0], throttle[0], x[0], z[0]
 
+    # TODO: add zero heading in solver
     new_init_states = [x - init_x, z - init_z, gs, psi + XPlaneDefs.zero_heading]
     desired_states = [desired_x, desired_z, desired_velocity]
-    controls, _, _ = solve_states(new_init_states, desired_states, acceleration_constraint,
-                                    turning_constraint, time_step=time_step, sim_time=num_steps)
+    winds = [wind_speed, (wind_direction + XPlaneDefs.zero_heading)]
+
+    controls, _, _ = solve_states(new_init_states, desired_states, winds, acceleration_constraint,
+                                  turning_constraint, time_step=time_step, sim_time=num_steps)
     controls = [[c[0], c[1] - XPlaneDefs.zero_heading] for c in controls]
-    rudder_controller.clear()
     throttle_controller.clear()
+    rudder_controller.clear()
     apply_controls(xp_client, throttle_controller, rudder_controller, controls, sample_time, time_step, receding_horizon)
