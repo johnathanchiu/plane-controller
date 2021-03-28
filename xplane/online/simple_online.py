1219ea0312d82d4416c1796ed8dc0d1b9d1fc219
john <johnathanchewy@gmail.com>
2021-03-28 16:06:48 -0700

    test new solver

---
+++ xplane/online/simple_online.py
@@ -0,0 +1,106 @@
+from simple_solver import solve_states
+from controller import apply_controls, takeoff, PID
+from definitions import XPlaneDefs
+from geometry import kn_to_ms, signed_rejection_dist, rotate
+
+from xpc import XPlaneConnect
+
+import numpy as np
+import random
+import math
+import time
+import sys
+
+### runway setup ###
+
+runway_origin_x = -25163.9477
+runway_origin_z = 33693.3450
+
+runway_end_x = -22742.57617
+runway_end_z = 31956.02344
+
+runway_heading = 54.331
+
+### states setup ###
+
+desired_x = runway_end_x
+desired_z = runway_end_z
+
+desired_velocity = 50 # m/s
+
+acceleration_constraint = 2 # m/s^2
+turning_constraint = 10 # degrees
+
+plane_cs = 27.41 # square meters
+plane_mass = 6175 * 0.45359237 # lbs -> kg
+plane_half_length = 10.82 / 2 # meters
+
+plane_specs = [plane_cs, plane_mass, plane_half_length]
+
+# solver time step
+time_step = 1 # seconds
+# solver number of states to solve
+num_steps = 4
+
+# PID controller recompute time step
+sample_time = 0.1
+# number of seconds to run the PID controller + solver
+simulation_steps = 50
+
+# recompute using solver after t seconds
+receding_horizon = 1
+
+# number of environments to sample
+num_environment_samples = 100
+
+TAKEOFF = True
+SAMPLE = False
+
+TAKEOFF_DIST = 500
+
+# create XPC object
+xp_client = XPlaneConnect()
+
+### initialize starting states ###
+
+origin_x = runway_origin_x
+origin_z = runway_origin_z
+
+start_x, start_y, start_z = xp_client.getDREFs(XPlaneDefs.position_dref)
+start_y = start_y[0]
+xp_client.sendDREFs(XPlaneDefs.position_dref, [origin_x, start_y, origin_z])
+
+# want to end here at t = sim_time
+desired_x -= origin_x
+desired_z -= origin_z
+
+time.sleep(1)
+
+xp_client.sendCOMM("sim/operation/fix_all_systems")
+# release park brake
+xp_client.sendDREF("sim/flightmodel/controls/parkbrake", 0)
+
+time.sleep(1)
+
+controls = None
+throttle_controller = PID(2.0, 0.0, 1.0, 10.0, sample_time)
+rudder_controller = PID(0.3, 0.4, 1.5, 10.0, sample_time)
+
+read_drefs = XPlaneDefs.control_dref + XPlaneDefs.position_dref
+winds = [[0, runway_heading + XPlaneDefs.zero_heading]]
+cl = np.array([desired_x, desired_y])
+
+for t in range(50):
+    gs, psi, throttle, x, _, z = xp_client.getDREFs(read_drefs)
+    gs, psi, throttle, x, z = gs[0], psi[0], throttle[0], x[0], z[0]
+    
+    desired_states = [runway_heading + XPlaneDefs.zero_heading, desired_velocity]
+    dist = signed_rejection_dist(cl, init_x, init_y)
+    init_states = [dist, gs, psi + XPlaneDefs.zero_heading]
+
+    controls, _ = solve_states(init_states, desired_states, cl, winds, plane_specs, acceleration_constraint,
+                                  turning_constraint, time_step=time_step, sim_time=num_steps)
+                                  
+                                  
+    controls = [[c[0], c[1] - XPlaneDefs.zero_heading] for c in controls]
+    apply_controls(xp_client, throttle_controller, rudder_controller, controls, sample_time, time_step, receding_horizon)
---
+++ xplane/simple_solver.py
@@ -0,0 +1,111 @@
+import numpy as np
+import scipy.optimize as opt
+
+from multiprocessing import Pool
+
+from geometry import signed_rejection_dist
+
+from definitions import XPlaneDefs
+
+import random
+import time
+
+
+get_states_controls = lambda x: [x[i+2:i+6] for i in range(0, len(x), 6)]
+
+
+def get_controls(states, time_step):
+    controls = []
+    prev_heading = states[0][1]
+    prev_velocity = states[0][0]
+    for state in states:
+        c_tup = (prev_velocity + state[-2] * time_step, prev_heading + state[-1] * time_step)
+        controls.append(c_tup)
+        prev_heading += state[-1]
+        prev_velocity += state[-2]
+    return controls
+
+
+def compute_states(init_state, controls, wind_dynamics, plane_specs, time_step=1):
+    
+    wind_speed, wind_heading = wind_dynamics
+    plane_cs, plane_mass, plane_half_length = plane_specs
+    wind_force = wind_speed * plane_cs
+    wind_acc = wind_force / plane_mass
+    
+    states = []
+    x, y, v, h = init_state
+
+    for i in range(0, len(controls), 2):
+        
+        a, w = controls[i:i+2]
+        states = np.concatenate((states, [x, y, v, h, a, w]))
+        
+        vx = (v * np.cos(np.radians(h))) + (time_step * wind_acc * np.cos(np.radians(wind_heading)))
+        vy = (v * np.sin(np.radians(h))) + (time_step * wind_acc * np.sin(np.radians(wind_heading)))
+        
+        x += time_step * vx
+        y += time_step * vy
+        
+        # rF * sin(theta)
+        wind_torque = plane_half_length * wind_force * np.sin(np.radians(wind_heading - h))
+        wind_w = 0.1 * time_step * (wind_torque / plane_mass) * plane_half_length
+        v += time_step * a
+        h += time_step * (w + wind_w)
+        
+        h %= 360
+
+    return states
+
+
+def formulate_objective(init_state, center_line, desired_states, environment, plane_specs, time_step=1):
+    
+    control_weight = 0.1
+    centerline_weight = 100
+    velocity_weight = 3
+    heading_weight = 5
+    
+    desired_h, desired_v = desired_states
+    
+    def objective(params):
+        cost = 0
+        for env in environment:
+            states = compute_states(init_state, params, env, plane_specs, time_step=time_step)
+            for i in range(6, len(states), 6):
+                px, py, v, h, a, w = states[i:i+6]
+                cost += centerline_weight * np.abs(signed_rejection_dist(center_line, px, py))
+                cost += velocity_weight * np.linalg.norm([desired_v - v], ord=2)
+                cost += heading_weight * np.linalg.norm([desired_h - h], ord=2)
+        return cost / len(environment)
+    
+    return objective
+
+
+def formulate_guess(sim_time):
+    return np.zeros((sim_time*2,))
+
+
+def solve_states(initial_states, desired_states, center_line, extern_conditions, plane_specs, acceleration_constraint, turning_constraint, time_step=1, sim_time=10):
+
+    rejection, init_velocity, init_heading = initial_states
+    desired_heading, desired_velocity = desired_states
+    
+    init_guess = formulate_guess(sim_time)
+    bounds = [(-acceleration_constraint, acceleration_constraint),
+              (-turning_constraint, turning_constraint)] * sim_time
+              
+    cl = np.array([desired_x, desired_y])
+    rinit_x, rinit_y = rotate(0, rejection, runway_heading + XPlaneDefs.zero_heading - 360)
+    state0 = [rinit_x, rinit_y, init_velocity, init_heading]
+
+    obj = formulate_objective(state0, cl, [runway_heading + zero_heading, desired_velocity],
+                              wind_dynamics, plane_specs, time_step=time_step)
+
+    result = opt.minimize(obj, init_guess, method='SLSQP', bounds=bounds,
+                          options={'eps': 0.3, 'maxiter': 500})
+    result
+    print('----', time.time() - start_time, 'seconds ----')
+
+    states = compute_states(state0, result.x, extern_conditions[0], plane_specs, time_step=time_step)
+    states = get_states_controls(states)
+    return get_controls(states, time_step), result.fun
